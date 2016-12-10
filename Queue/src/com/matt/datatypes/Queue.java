d58a16f21572b6caf67b9dc53edf1d9c76b2cfff
mstrong <mstrong1341@gmail.com>
2016-12-10 13:28:54 -0500

    implementing a queue in java

---
+++ Queue/src/com/matt/datatypes/Queue.java
@@ -0,0 +1,48 @@
+package com.matt.datatypes;
+
+
+import java.util.LinkedList;
+
+public class Queue {
+
+    LinkedList queue;
+
+    // Constructor. Making a queue instance.
+    public Queue() {
+        queue = new LinkedList();
+    }
+
+    // Methods
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public void enqueue(int n) {
+        queue.addLast(n);
+    }
+
+    public int dequeue() {
+        return (int) queue.removeFirst();
+    }
+
+    public int peek() {
+        return (int) queue.getFirst();
+    }
+
+    public static void main(String args[]) {
+        Queue numberQueue = new Queue();
+
+        numberQueue.enqueue(5);
+        numberQueue.enqueue(7);
+        numberQueue.enqueue(6);
+
+        System.out.println("1st out: " + numberQueue.dequeue());
+        System.out.println("Peek at 2nd item: " + numberQueue.peek());
+        System.out.println("2nd out: " + numberQueue.dequeue());
+        System.out.println("3rd out: " + numberQueue.dequeue());
+    }
+}
